157f4af279a8c5aeaa370758edac578c4898d789
Mahmoud Shehata <mmshehatta@gmail.com>
2021-09-22 08:47:47 +0200

    Chapter 5

---
+++ ch5/code/backend/blog_project/urls.py
@@ -0,0 +1,9 @@
+# blog_project/urls.py
+from django.contrib import admin
+from django.urls import include, path # new
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('posts.urls')), # new
+]
